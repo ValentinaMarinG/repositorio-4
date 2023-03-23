be6ec61369e84114b3a47dbc4f0ea93ad95633f3
ValentinaMarinG <valentinamaringalvis28@gmail.com>
2023-03-22 22:22:22 -0500

    Modificaciones en la estrutura del proyecto

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function main(){
+    suma=100+1;
+    console.log("Esto es una prueba de manejo de repositorio");
+    console.log("Me va a enloquecer git");
+    console.log("Esto es una suma "+suma);
+}
